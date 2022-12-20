4bd2c0c99982d4213ab4318334bebdaaebf31117
GuilhermeVFSousa <guilhermevfsousa@gmail.com>
2022-12-19 22:41:30 -0300

    'create' endpoint create for Tecnico

--- src/main/java/com/gvfs/helpdesk/services/TecnicoService.java
+++ src/main/java/com/gvfs/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gvfs.helpdesk.domain.Tecnico;
+import com.gvfs.helpdesk.domain.dtos.TecnicoDTO;
 import com.gvfs.helpdesk.repositories.TecnicoRepository;
 import com.gvfs.helpdesk.services.exceptions.ObjectNotFoundException;
 
@@ -23,6 +24,12 @@ public class TecnicoService {
 
 	public List<Tecnico> findAll() {
 		return repository.findAll();
+	}
+
+	public Tecnico create(TecnicoDTO objDTO) {
+		objDTO.setId(null);
+		Tecnico newObj = new Tecnico(objDTO);
+		return repository.save(newObj);
 	}
 
 }
