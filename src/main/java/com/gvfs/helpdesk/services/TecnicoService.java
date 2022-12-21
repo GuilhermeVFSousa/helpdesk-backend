b290c7a7a6f3461531001a531ee23929ef5d1062
GuilhermeVFSousa <guilhermevfsousa@gmail.com>
2022-12-20 22:00:02 -0300

    create put endpoint for Tecnico

--- src/main/java/com/gvfs/helpdesk/services/TecnicoService.java
+++ src/main/java/com/gvfs/helpdesk/services/TecnicoService.java
@@ -3,6 +3,8 @@ package com.gvfs.helpdesk.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -23,7 +25,7 @@ public class TecnicoService {
 	@Autowired
 	private PessoaRepository pessoaRepository;
 	
-	public Tecnico fidnById(Integer id) {
+	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
 		return obj.orElseThrow(() ->  new ObjectNotFoundException("Objeto não encontrado! Id: " + id));
 	}
@@ -37,6 +39,14 @@ public class TecnicoService {
 		validaPorCpfEEmail(objDTO);
 		Tecnico newObj = new Tecnico(objDTO);
 		return repository.save(newObj);
+	}
+	
+	public Tecnico update(Integer id, @Valid TecnicoDTO objDTO) {
+		objDTO.setId(id);
+		Tecnico oldObj = findById(id);
+		validaPorCpfEEmail(objDTO);
+		oldObj = new Tecnico(objDTO);
+		return repository.save(oldObj);
 	}
 
 	private void validaPorCpfEEmail(TecnicoDTO objDTO) {
