1173dc0fa3e5d65551714c62b8288bbe1f1e41d3
GuilhermeVFSousa <guilhermevfsousa@gmail.com>
2022-12-21 17:33:22 -0300

    add post method for Chamado

--- src/main/java/com/gvfs/helpdesk/resources/ChamadoResource.java
+++ src/main/java/com/gvfs/helpdesk/resources/ChamadoResource.java
@@ -1,13 +1,19 @@
 package com.gvfs.helpdesk.resources;
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.gvfs.helpdesk.domain.Chamado;
 import com.gvfs.helpdesk.domain.dtos.ChamadoDTO;
@@ -31,6 +37,13 @@ public class ChamadoResource {
 		List<Chamado> list = service.findAll();
 		List<ChamadoDTO> listDTO = list.stream().map(obj -> new ChamadoDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	@PostMapping
+	public ResponseEntity<ChamadoDTO> create(@Valid @RequestBody ChamadoDTO obj) {
+		Chamado newObj = service.create(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newObj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 }
